f61ca0740a2e82ff8c288b1c0e8ae9d2fa81b352
Vidhan Mangla <vidhanmangla@gmail.com>
2022-06-21 05:52:58 +0100

    master

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,13 +11,13 @@ const Projects = () => {
             <div className="projects__cards">
 
                 <div className="projects__card">
-                    <h3>Uniswap</h3>
+                    <h3>Cryptocurrency Swap Exchange</h3>
                     <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>One
+                    <p>A full-stack blockchain application. An instant cryptocurrency exchange powered by the ethereum blockchain. It allows to buy and sell JIZZY Token at a fixed price.
                     </p>
 
-                    <a href="https://uniswap.org/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/Uniswap/v2-core" target="_blank" className="button">Code</a>
+                    <a href="https://euphonious-axolotl-704b66.netlify.app/" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vidhanmangla/Cryptocurrency-Swap-Exchange" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
