9a32f0ff823fb76601b94b6b069f4a01c2f3b43d
Vidhan Mangla <vidhanmangla@gmail.com>
2022-06-22 17:10:12 +0100

    master

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ const About = () => {
             <h2>About Me</h2>
             <p>
             Smart Contract developer skilled at writing, testing, deploying, and administering smart contracts by taking care of gas
-optimisation and security.I keep myself updated on everything related to blockchain technology and Web3, and taught
-myself to become a developer.<br></br>
+optimisation and security. I keep myself updated on everything related to blockchain technology and Web3, and taught myself to become a 
+developer.<br></br>
 </p>
 <p>
-Recognised as a pressure-coping racing driver with a calm demeanour who spends his free time as a musician
-            </p>
+Recognised as a pressure-coping racing driver with a calm demeanour who spends his free time as a musician.
+            </p><br></br>
             <p>“Loyalty is a two-way street. If I’m asking for it from you, then you’re getting it from me.”</p>
         </section>
     );
