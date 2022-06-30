d793e0ce67c3a767efdf7087105eb390901e64c2
Vidhan Mangla <vidhanmangla@gmail.com>
2022-06-30 11:32:34 +0100

    master

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 // Import Assets
-import uniswap from '../assets/uniswap.png';
+import jizzyswap from '../assets/jizzyswap.png';
+import nft_marketplace from '../assets/nft_marketplace.png';
+import raffle from '../assets/raffle.jpg';
 
 const Projects = () => {
     return (
@@ -10,8 +12,11 @@ const Projects = () => {
 
                 <div className="projects__card">
                     <h3>Cryptocurrency Swap Exchange</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
+                    <img src={jizzyswap} alt="Uniswap Swap Page" />
                     <p>A full-stack blockchain application. An instant cryptocurrency exchange powered by the Ethereum blockchain. It allows users to buy and sell JIZZY tokens at a fixed price against ether.
+                    </p>
+                    <p>
+                        Live on the Rinkeby testnet.<br></br>Will deploy on the Ethereum-mainnet soon.
                     </p>
 
                     <a href="https://celadon-banoffee-0eef4e.netlify.app/" target="_blank" className="button">Site</a>
@@ -19,23 +24,23 @@ const Projects = () => {
                 </div>
 
                 <div className="projects__card">
-                    <h3>Cryptocurrency Swap Exchange</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>A full-stack blockchain application. An instant cryptocurrency exchange powered by the Ethereum blockchain. It allows users to buy and sell JIZZY tokens at a fixed price against ether.
+                    <h3>Decentralised Lottery (Raffle)</h3>
+                    <img src={raffle} alt="Uniswap Swap Page" />
+                    <p>Upcoming...<br></br>(Within this month.)
                     </p>
 
-                    <a href="https://celadon-banoffee-0eef4e.netlify.app/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vidhanmangla/jizzyb_ether_swap_exchange" target="_blank" className="button">Code</a>
+                    <a href="" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vidhanmangla/lottery_raffle" target="_blank" className="button">Code</a>
                 </div>
 
                 <div className="projects__card">
-                    <h3>Cryptocurrency Swap Exchange</h3>
-                    <img src={uniswap} alt="Uniswap Swap Page" />
-                    <p>A full-stack blockchain application. An instant cryptocurrency exchange powered by the Ethereum blockchain. It allows users to buy and sell JIZZY tokens at a fixed price against ether.
+                    <h3>Non-fungible Token (NFT) Marketplace</h3>
+                    <img src={nft_marketplace} alt="Uniswap Swap Page" />
+                    <p>Upcoming...<br></br>(Within this month.)
                     </p>
 
-                    <a href="https://celadon-banoffee-0eef4e.netlify.app/" target="_blank" className="button">Site</a>
-                    <a href="https://github.com/vidhanmangla/jizzyb_ether_swap_exchange" target="_blank" className="button">Code</a>
+                    <a href="" target="_blank" className="button">Site</a>
+                    <a href="https://github.com/vidhanmangla/nft_marketplace" target="_blank" className="button">Code</a>
                 </div>
                 
             </div>
