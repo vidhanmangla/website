395202ae58f798b9a69a7071c6043720bfd21b02
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-04 21:42:51 +0100

    master

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,9 +3,11 @@ const Social = () => {
         <div className="social">
             <h2>Socials</h2>
 
+            <a href="https://www.instagram.com/_jizzy_b_" className="button">Instagram (Racing)</a>
             <a href="https://www.linkedin.com/in/vidhanmangla" className="button">LinkedIn</a>
             <a href="https://github.com/vidhanmangla?tab=repositories" className="button">GitHub</a>
             <a href="https://twitter.com/vidhanmangla" className="button">Twitter</a>
+            <a href="https://www.instagram.com/fireside.on.an.island" className="button">Instagram (Music)</a>
         </div>
     );
 }
