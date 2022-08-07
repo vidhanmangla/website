e4604ba3239c4d9b366956689744451eabffc9cc
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-07 14:10:56 +0100

    flora

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,14 +5,14 @@ const Experience = () => {
 
             <ul>
                 <li>
-                    <strong>Smart Contract Developer, The Golden Egg, Guildford, United Kingdom - (June 2022 – present)</strong>
+                    <strong>Smart Contract Developer, The Golden Egg, Guildford, England (June 2022 – present)</strong>
                     <ul>
                         <li>Helped the company launch its initial stage projects.</li>
                         <li>Developed and tested gas optimised and secure Smart Contracts before deploying them as full-stack decentralised applications on the Ethereum mainnet.</li>
                     </ul>
                 </li>
                 <li>
-                    <strong>Master of Science, FinTech and Policy, University of Surrey - (September 2021 – September 2022)</strong>
+                    <strong>Master of Science, FinTech and Policy, University of Surrey, England (September 2021 – September 2022)</strong>
                     <ul>
                         <li>Studied DApp development, assisted Research &amp; Development on Web3 Products.</li>
                         <li>Researched areas including Blockchain, De-Fi, alternative lending, InsurTech and RegTech in the UK and globally.</li>
@@ -22,7 +22,7 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <strong>Assistant Manager, Kotak Mahindra Bank, New Delhi, India - (March 2020 – August 2020)</strong>
+                    <strong>Assistant Manager, Kotak Mahindra Bank, India (March 2020 – August 2020)</strong>
                     <ul>
                         <li>Generated business revenue by selling insurance to customers through proper planning and strategic execution.</li>
                         <li>Determined skill gaps and optimised workplace competencies, increasing employee morale and productivity whilst supporting the company in conducting in-house training programmes.</li>
@@ -30,14 +30,14 @@ const Experience = () => {
                     </ul>
                 </li>
                 <li>
-                    <strong>Bachelor of Technology, Information Technology, GGSIP University, India - (August 2015 – July 2019)</strong>
+                    <strong>Bachelor of Technology, Information Technology, GGSIP University, India (August 2015 – July 2019)</strong>
                     <ul>
                         <li>Obtained detailed knowledge of common algorithms, data structures, and their computational and memory complexities, as well as experience of implementing them from scratch.</li>
                         <li>Attained leading industry skills, including object-oriented programming (OOP), database management systems, and algorithms design and analysis.</li>
                     </ul>
                 </li>
                 <li>
-                    <strong>Cloud Computing Intern, ESDS Software Solutions, New Delhi, India - (July 2018 – August 2018)</strong>
+                    <strong>Cloud Computing Intern, ESDS Software Solutions, India (July 2018 – August 2018)</strong>
                     <ul>
                         <li>Researched various cloud platforms available in the market and the technology used in data centres.</li>
                         <li>Worked with the sales and marketing team and accompanied senior officers on official visits.</li>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
             <p><small>This Web 3.0 portfolio website is proudly hosted on IPFS</small></p>
             <br></br>
             <p><small>Copyright &copy; 2022 by Vidhan Mangla</small></p>
+            <p><small>All thanks, Flora</small></p>
         </footer>
     );
 }
