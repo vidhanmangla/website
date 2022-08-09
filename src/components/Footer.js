28f6e276b82dc79f068580b796568769b8b600b5
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-09 20:28:54 +0100

    master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <p><small>This Web 3.0 portfolio website is proudly hosted on IPFS</small></p>
-            <br></br>
             <p><small>Copyright &copy; 2022 by Vidhan Mangla</small></p>
         </footer>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,11 +14,13 @@ const Header = () => {
                         <a href='mailto:vidhanmangla@gmail.com' className='button'>Get in Touch!</a>
                 <br></br>
                 <br></br>
-                <br></br>
                         <small><a href ="https://link.storjshare.io/jxz5rogc7wkkffo4qthkbmrsetaq/jobs-material%2FResume.pdf" attributes-list>Resume</a></small>
                 <br></br>
                 <br></br>
                         <small><a href ="https://link.storjshare.io/jx6g476ckgkd6epgnedfskoflzca/jobs-material%2FCover%20Letter.pdf" attributes-list>Cover Letter</a></small>
+                <br></br>
+                <br></br>
+                        <small>This Web 3.0 portfolio website is proudly hosted on IPFS</small>
             </div>
         </section>
     );
