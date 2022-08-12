fde51ecea9d8f67169f621a56263533f6f212ce3
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-12 04:19:34 +0100

    master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p><small>Copyright &copy; 2022 by Vidhan Mangla</small></p>
+            <br></br>
+            <p><small>All thanks, Flora</small></p>
         </footer>
     );
 }
