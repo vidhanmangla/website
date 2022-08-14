6bf3c082702c485a8aaff1fd0bf8eded9b5deb89
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-14 12:11:13 +0100

    master

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,8 +7,9 @@ const Experience = () => {
                 <li>
                     <strong>Smart Contract Developer, The Golden Egg, Guildford, England (Part-time) (June 2022 – present)</strong>
                     <ul>
-                        <li>Helped the company launch its initial stage projects.</li>
-                        <li>Developed and tested gas optimised and secure Smart Contracts before deploying them as full-stack decentralised applications on the Ethereum mainnet.</li>
+                        <li>Advisor to the CEO and CTO of the company on issues related to Blockchain development.</li>
+                        <li>Worked for the company planning its initial stage projects.</li>
+                        <li>Developed and tested gas optimised and secure Smart Contracts. Developed full-stack Blockchain Applications.</li>
                     </ul>
                 </li>
                 <li>
@@ -23,25 +24,12 @@ const Experience = () => {
                 </li>
                 <li>
                     <strong>Assistant Manager, Kotak Mahindra Bank, India (March 2020 – August 2020)</strong>
-                    <ul>
-                        <li>Generated business revenue by selling insurance to customers through proper planning and strategic execution.</li>
-                        <li>Determined skill gaps and optimised workplace competencies, increasing employee morale and productivity whilst supporting the company in conducting in-house training programmes.</li>
-                        <li>Ensured fulfilling applicable banking and national financial regulations and policies.</li>
-                    </ul>
                 </li>
                 <li>
                     <strong>Bachelor of Technology, Information Technology, GGSIP University, India (August 2015 – July 2019)</strong>
-                    <ul>
-                        <li>Obtained detailed knowledge of common algorithms, data structures, and their computational and memory complexities, as well as experience of implementing them from scratch.</li>
-                        <li>Attained leading industry skills, including object-oriented programming (OOP), database management systems, and algorithms design and analysis.</li>
-                    </ul>
                 </li>
                 <li>
                     <strong>Cloud Computing Intern, ESDS Software Solutions, India (July 2018 – August 2018)</strong>
-                    <ul>
-                        <li>Researched various cloud platforms available in the market and the technology used in data centres.</li>
-                        <li>Worked with the sales and marketing team and accompanied senior officers on official visits.</li>
-                    </ul>
                 </li>
             </ul>
         </div>
