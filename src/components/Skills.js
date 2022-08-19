e4a91886fd966b8ea8937c8f861a6168a2b88047
Vidhan Mangla <vidhanmangla@gmail.com>
2022-08-19 09:57:31 +0100

    master

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,27 +3,29 @@ const Skills = () => {
         <section className='skills'>
             <h2>Technical Skills</h2>
             <p>
-                <li>Deep conceptual and technological understanding of Ethereum (EVM), including the Blockchain Oracle.</li>
+                        <li>Deep conceptual and technological understanding of Ethereum (EVM), including the Blockchain Oracle.</li>
                 <br></br>
-                <li>Implement and deploy smart contracts on Ethereum mainnet and layer 2’s.</li>
+                        <li>Implement and deploy smart contracts on Ethereum mainnet and layer 2’s.</li>
                 <br></br>
-                <li><strong>Languages:</strong> Solidity (Tokens + NFTs + DAOs), JavaScript (Node.js, React, Next.js), HTML and CSS.</li>
+                        <li><strong>Languages:</strong> Solidity (Tokens + NFTs + DAOs), JavaScript (Node.js, React, Next.js), HTML and CSS.</li>
                 <br></br>
-                <li><strong>Development Environments:</strong> Hardhat, Remix, Truffle Framework and Ganache.</li>
+                        <li><strong>Development Environments:</strong> Hardhat, Remix, Truffle Framework and Ganache.</li>
                 <br></br>
-                <li>Comfortable with using OpenZeppelin’s library of smart contracts.</li>
+                        <li>Comfortable with using OpenZeppelin’s library of smart contracts.</li>
                 <br></br>
-                <li>Working knowledge of Moralis, experience creating subgraphs on The Graph.</li>
+                        <li>Working knowledge of Moralis, experience creating subgraphs on The Graph.</li>
                 <br></br>
-                <li><u>ERC-20</u>: Fungible Tokens, <u>ERC-721</u>: Non-Fungible Tokens, <u>ERC-777</u>: Token standard, <u>ERC-1155</u>: Multi-token standard.</li>
+                        <li>General understanding of cryptography for Blockchain.</li>
                 <br></br>
-                <li><u>ERC-1400</u>: Security Token Standard and <u>ERC-4626</u>: Tokenised Vault Standard contracts.</li>
+                        <li>Infura and Alchemy (Blockchain nodes), knowledge of zkSync, zk-SNARK and zkEVM (Zero-Knowledge proofs).</li>
                 <br></br>
-                <li>Infura and Alchemy (Blockchain nodes), knowledge of zkSync, zk-SNARK and zkEVM (Zero-Knowledge proofs).</li>
+                        <li>Git and Moralis (version control and APIs), Mythril and Coverage (Security and Gas efficiency).</li>
                 <br></br>
-                <li>Git and Moralis (version control and APIs), Mythril and Coverage (Security and Gas efficiency).</li>
+                        <li><u>ERC-20</u>: Fungible Tokens, <u>ERC-721</u>: Non-Fungible Tokens, <u>ERC-777</u>: Token standard, <u>ERC-1155</u>: Multi-token standard.</li>
                 <br></br>
-                <li>Financial Softwares: Bloomberg Terminal, Capital IQ, Microsoft Suite.</li>
+                        <li><u>ERC-1400</u>: Security Token Standard and <u>ERC-4626</u>: Tokenised Vault Standard contracts.</li>
+                <br></br>
+                        <li>Financial Softwares: Bloomberg Terminal, Capital IQ, Microsoft Suite.</li>
             </p>
         </section>
     );
