866e0e57f6184455e6237c039ed4d11cf5f2dd21
Vidhan Mangla <vidhanmangla@gmail.com>
2022-09-02 22:49:36 +0100

    master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ const Footer = () => {
 
         <footer className="footer">
 
-            <p><small>Copyright &copy; 2022 by Vidhan Mangla.</small></p>
             <p><small>This Web 3.0 portfolio website is proudly hosted on IPFS</small></p>
             
         </footer>
