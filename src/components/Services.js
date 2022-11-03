abcb04b1bdc1636279d1dfc733ad19d5c29ecfe0
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2022-11-03 11:34:31 +0000

    master

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,13 +5,13 @@ const Services = () => {
 
             <h2>Services I Provide</h2>
             <p>
-                        <li>Custom blockchain development & integration. • Enterprise blockchain development. • Blockchain consulting & advisory.</li>
+                        <li>Blockchain game development. • Smart contract development & audit. • Blockchain training for tech & business insiders.</li>
                 <br></br>
                         <li>NFT consulting & development. • DApp development. • DeFi solutions engineering.</li>
                 <br></br>
                         <li>Cryptocurrency wallet development. • Hyperledger solutions development. • Cryptocurrency exchange engineering.</li>
                 <br></br>
-                        <li>Blockchain game development. • Smart contract development & audit. • Blockchain training for tech & business insiders.</li>
+                        <li>Custom blockchain development & integration. • Enterprise blockchain development. • Blockchain consulting & advisory.</li>
             </p>
 
         </section>
