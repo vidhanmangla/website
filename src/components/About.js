ad9bc34a549604d16dd282351ffc86a636de1018
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2022-12-19 13:19:02 +0000

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ const About = () => {
         <section className='about'>
 
             <h2>About Me</h2>
-            <p>
+            <p align="center">
                 A crypto enthusiast with experience developing dApps on EVM based blockchains with 1+ year of expertise, skilled
                 at writing, testing, deploying and auditing Smart Contracts by taking care of gas optimisation and security.
             </p>
                 <br></br>
                 <br></br>
-            <p>
+            <p align="center">
                 A pressure-coping motor-racing driver with a calm demeanour, spends his free time as a musician.
                 I am a passionate man, and passion drives me. Everything I do in life, it's my passion!
             </p>
