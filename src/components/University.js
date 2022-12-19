ea21e4c59e251abca663221a4a630a7551db31cf
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2022-12-19 13:26:54 +0000

    Update University.js

--- src/components/University.js
+++ src/components/University.js
@@ -3,27 +3,27 @@ const University = () => {
 
         <div className="university">
 
-            <h2>University Projects</h2>
+            <h2 align="center">University Projects</h2>
 
             <ul>
                 
-                <li>
+                <li align="center">
                 <a href ="https://link.storjshare.io/jx5e7apad6kd5yvz3kns2rf27oza/jobs-material%2FFinTech%20Project%20.pdf" attributes-list>FinTech and Policy Project</a>: Conducted an independent research and completed a report on the cryptocurrency firm Abra. 
                 </li>
 
-                <li>
+                <li align="center">
                 <a href ="https://link.storjshare.io/jxhzwbtgs4rp5quqnwwmlu6nruya/jobs-material%2FCases%20in%20FinTech%20%26%20Policy.pdf" attributes-list>Cases in FinTech and Policy</a>: An individual report that discusses the business model and prospects of Binance.
                 </li>
 
-                <li>
+                <li align="center">
                 <a href ="https://link.storjshare.io/juwky6nhpzrbauofnsovxjxciaia/jobs-material%2FFinancial%20Regulation%20%26%20FinTech%20Policy.pdf" attributes-list>Financial Regulation and FinTech Policy</a>: Research done on financial regulation of digital assets.
                 </li>
 
-                <li>
+                <li align="center">
                 <a href ="https://link.storjshare.io/jvhhim3jzdzfhnv3k27oppq4es7a/jobs-material%2FCyberlaw.pdf" attributes-list>Cyberlaw</a>: Whether Artificial Intelligence should be regulated in different ways in different jurisdictions.
                 </li>
                 
-                <li>
+                <li align="center">
                 <a href ="https://link.storjshare.io/jvslxi6jyn2trcwi6zbuvxkcxteq/jobs-material%2FEthics%20of%20Artificial%20Intelligence.pdf" attributes-list>Artificial Intelligence</a>: How developers and consumers alike can help reduce algorithmic bias and unfairness in algorithms.
                 </li>
             
