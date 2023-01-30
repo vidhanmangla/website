044fd1c587ca2ee2800381a6f51095e994fdc4f9
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-01-30 14:31:13 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
                 
                 <br></br>
 
-                        <p><small>Copyright © 2023 Vidhan Mangla. All Rights Reserved.</small></p>
+                        <p><small>© 2023 Vidhan Mangla. All Rights Reserved.</small></p>
             
         </footer>
         
