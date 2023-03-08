7e7288135e33598c0ec9f8a1bc6de1bfa297a1df
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-03-08 18:43:19 +0000

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -113,7 +113,7 @@ const Projects = () => {
                                         Live on the Goerli testnet.
                                     </p>
 
-                                <a align="center" href="https://master.d5615dcxchqd7.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
+                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
                     
                         </div>
 
