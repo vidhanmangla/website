7946269d534db5dc5117ebe9453f21e3568f3de4
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-03-23 14:30:32 +0000

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -29,7 +29,7 @@ const Blog = () => {
                         <br></br>
                             <p>We create inflation by printing money like newspaper, we take money out of pocket of the poor and all we’ve got left to do is give out a public statement,</p>
                         <br></br>
-                            <p>“Bank of England in a desperate attempt to save your pocketmoney raises interest rates to fight inflation.”</p>
+                            <p>“Bank of England in a desperate attempt to save the economy raises interest rates to fight inflation.”</p>
                         <br></br>
                             <p>Inflation the enemy, who created it? Us! Give out a public statement,</p>
                         <br></br>
