ccfa92fa956ad2cb7eef46f8607a6fa7706ff626
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-03-23 15:32:38 +0000

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -60,9 +60,7 @@ const Blog = () => {
                     <footer align="center" className="footer">
 
                             <p><small>This Web 3.0 website is proudly hosted on IPFS</small></p>
-
                         <br></br>
-
                             <p><small>Copyright © 2023 Vidhan Mangla.</small></p>
 
                     </footer>
