7b67cb1b48b86ba97f26b9844e16529298ad2370
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-04-07 01:26:51 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <br></br>
 
                         <p><small>Copyright © 2023 Vidhan Mangla.</small></p>
-                        <small><a href ="https://link.storjshare.io/jxwdhp75vxdnkx3zgeeovvobtbqq/jobs-material%2Fbitcoin.pdf" target="_blank" rel="noreferrer" attributes-list>Bitcoin Whitepaper</a> | <a href ="" target="_blank" rel="noreferrer" attributes-list>Ethereum Yellowpaper</a></small>
+                        <small><a href ="https://link.storjshare.io/jxwdhp75vxdnkx3zgeeovvobtbqq/jobs-material%2Fbitcoin.pdf" target="_blank" rel="noreferrer" attributes-list>Bitcoin Whitepaper</a> | <a href ="https://link.storjshare.io/jxjrs5eyhosfwty3widjvna22a4q/jobs-material%2Fethereum.pdf" target="_blank" rel="noreferrer" attributes-list>Ethereum Yellowpaper</a></small>
             
         </footer>
         
