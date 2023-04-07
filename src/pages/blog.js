4c791136cb46c5f850fedbdad45aae7ab3cb15e6
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-04-07 01:19:36 +0100

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -57,7 +57,7 @@ const Blog = () => {
                             <p><small>This Web 3.0 website is proudly hosted on IPFS</small></p>
                         <br></br>
                             <p><small>Copyright © 2023 Vidhan Mangla.</small></p>
-                            <small><a href ="https://link.storjshare.io/jxyw3i564fhyvewk5xr33i7hxvzq/jobs-material%2FBitcoin%20Whitepaper.pdf" target="_blank" rel="noreferrer" attributes-list>Bitcoin Whitepaper</a></small>
+                            <small><a href ="https://link.storjshare.io/jxwdhp75vxdnkx3zgeeovvobtbqq/jobs-material%2Fbitcoin.pdf" target="_blank" rel="noreferrer" attributes-list>Bitcoin Whitepaper</a></small>
 
                     </footer>
 
