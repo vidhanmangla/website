c14a10b4af6b29d377dced3227d1751bd08c5668
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-04-13 01:55:48 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,6 @@
-import jizzyswap from '../assets/jizzyswap.png'
 import marketplace from '../assets/marketplace.png'
 import raffle from '../assets/raffle.png'
 import nfts from '../assets/nfts.png'
-import dao from '../assets/dao.png'
-import DeFi_Bank from '../assets/DeFi_Bank.png'
-import blockchain from '../assets/blockchain.png'
-import learn_blockchain from '../assets/learn_blockchain.png'
-import keys from '../assets/keys.png'
 
 
 const Projects = () => {
@@ -18,86 +12,6 @@ const Projects = () => {
             <h2>Technical Projects</h2>
 
                     <div className="projects__cards">
-
-
-                        <div className="projects__card">
-
-                            <h3>Blockchain Demo</h3>
-                            
-                                <img src={learn_blockchain} alt="Learn_Blockchain" />
-                            
-                                    <p>
-                                        A web-based demonstration of blockchain concepts.
-                                    </p>
-
-                                <a align="center" href="https://github.com/vidhanmangla/blockchain-demo" target="_blank" rel="noreferrer" className="button">Learn PoW Blockchain</a>
-
-                        </div>
-                        
-
-                        <div className="projects__card">
-
-                            <h3>Public / Private Keys</h3>
-                            
-                                <img src={keys} alt="Keys" />
-                                    <p>
-                                        A web-based demonstration of public/private key pair concepts.
-                                    </p>
-
-                                <a align="center" href="https://github.com/vidhanmangla/public-private-key-demo" target="_blank" rel="noreferrer" className="button">Learn Crypto Signature</a>
-
-                        </div>
-
-                        
-                        <div className="projects__card">
-
-                            <h3>Blockchain</h3>
-                            
-                                <img src={blockchain} alt="Blockchain" />
-                            
-                                    <p>
-                                        A minimalistic blockchain using proof-of-work consensus based on Bitcoin.
-                                    </p>
-
-                                <a align="center" href="https://github.com/vidhanmangla/Jizzy_Blockchain" target="_blank" rel="noreferrer" className="button">Code</a>
-
-                        </div>
-
-
-                        <div className="projects__card">
-
-                            <h3>Liquidity Farming</h3>
-    
-                                <img src={DeFi_Bank} alt="DeFi Bank" />
-        
-                                    <p>
-                                        Users can earn rewards by staking their stablecoin tokens.
-                                    </p>
-                                    <p>
-                                        Available on the Sepolia testnet.
-                                    </p>
-
-                                <a align="center" href="https://jocular-dolphin-7b0f16.netlify.app/" target="_blank" rel="noreferrer" className="button">Go to website</a>
-
-                        </div>
-
-
-                        <div className="projects__card">
-
-                            <h3>Crypto Exchange</h3>
-                            
-                                <img src={jizzyswap} alt="Jizzy Swap Page" />
-                            
-                                    <p>
-                                        An instant cryptocurrency exchange powered by the Ethereum blockchain.
-                                    </p>
-                                    <p>
-                                        Available on the Goerli testnet.
-                                    </p>
-
-                                <a align="center" href="https://luminous-sunflower-a18391.netlify.app/" target="_blank" rel="noreferrer" className="button">Go to website</a>
-                    
-                        </div>
 
 
                         <div className="projects__card">
@@ -150,21 +64,6 @@ const Projects = () => {
                                     </p>
 
                                 <a href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
-
-                        </div>
-
-
-                        <div className="projects__card">
-
-                            <h3>Revolutionary DAO</h3>
-                            
-                                <img src={dao} alt="DAO" />
-                            
-                                    <p>
-                                        A decentralised autonomous organisation inspired by Compound which has 100% on-chain voting and governance. 
-                                    </p>
-                            
-                                <a align="center" href="https://github.com/vidhanmangla/decentralised-autonomous-organisation" target="_blank" rel="noreferrer" className="button">Code</a>
 
                         </div>
 
