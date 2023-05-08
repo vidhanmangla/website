a25848d0f476cbde668ad963e909599faa23511e
vidhanmangla <vidhanmangla@gmail.com>
2023-05-08 20:35:49 +0100

    added zkp

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -12,9 +12,7 @@ const Certifications = () => {
                 <p>
 
                             <p align="center"><a href="https://www.credential.net/270c2948-0889-4534-a811-f6fad25a4c72#gs.lrhojt" target="_blank" rel="noreferrer">Certified Blockchain Developer™</a> | <a href="https://portal.bloombergforeducation.com/certificates/4cntDQVooA9Dn84gLpyV1Mae" target="_blank" rel="noreferrer">Bloomberg Market Concepts</a></p>
-
                     <br></br>
-
                             <img align="center" src={Certificates} alt="Certificates" width="650" height="250"></img>
 
                 </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import marketplace from '../assets/marketplace.png'
 import raffle from '../assets/raffle.png'
 import nfts from '../assets/nfts.png'
+import zkp from '../assets/zkp.png'
 
 
 const Projects = () => {
@@ -15,54 +16,48 @@ const Projects = () => {
 
 
                         <div className="projects__card">
-
-                            <h3>Lottery!</h3>
-                            
-                                <img src={raffle} alt="Raffle Page" />
-                            
+                            <h3>zk Documents</h3>
+                                <img src={zkp} alt="zk proof" />
                                     <p>
-                                        Lottery powered by the Ethereum blockchain and Chainlink oracle.
+                                        Verification of documents without revealing the information in it by using zero-knowledge cryptography.
                                     </p>
-                                    <p>
-                                        Available on Goerli and Sepolia testnet.
-                                    </p>
-
-                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
-                    
+                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Available Locally</a>
                         </div>
 
                         <div className="projects__card">
-
                             <h3>Non Fungible Tokens</h3>
-                            
                                 <img src={nfts} alt="NFTs" />
-                            
                                     <p>
                                         A collection of NFT products created by Vidhan.
                                     </p>
                                     <p>
                                         Available on the marketplace soon.
                                     </p>
-
                                 <a align="center" href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Marketplace (Soon)</a>
-                        
                         </div>
 
                         <div className="projects__card">
-
                             <h3>Marketplace</h3>
-                            
                                 <img src={marketplace} alt="Marketplace" />
-                            
                                     <p>
                                         A web3 marketplace to buy, sell and trade digital products.
                                     </p>
                                     <p>
                                         Available on Ethereum mainnet.
                                     </p>
-
                                 <a href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
+                        </div>
 
+                        <div className="projects__card">
+                            <h3>Lottery!</h3>
+                                <img src={raffle} alt="Raffle Page" />
+                                    <p>
+                                        Lottery powered by the Ethereum blockchain and Chainlink oracles.
+                                    </p>
+                                    <p>
+                                        Available on Goerli and Sepolia testnet.
+                                    </p>
+                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
                         </div>
 
 
