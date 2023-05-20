1686c39eab7dec905fb11e8afb772be59a969df6
Vidhan Mangla <vidhanmangla@gmail.com>
2023-05-20 06:13:35 +0100

    master

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import marketplace from '../assets/marketplace.png'
 import raffle from '../assets/raffle.png'
 import nfts from '../assets/nfts.png'
 import zkp from '../assets/zkp.png'
+import tickets from '../assets/nft-ticketing.jpg'
 
 
 const Projects = () => {
@@ -19,9 +20,30 @@ const Projects = () => {
                             <h3>zk Documents</h3>
                                 <img src={zkp} alt="zk proof" />
                                     <p>
-                                        Verification of documents without revealing the information in it by using zero-knowledge cryptography.
+                                        Verification of documents without revealing the information in it by using zero-knowledge cryptography (ZK-SNARK) on the Ethereum blockchain.
                                     </p>
-                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Available Locally</a>
+                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Contact To Enquire</a>
+                        </div>
+
+                        <div className="projects__card">
+                            <h3>Lottery!</h3>
+                                <img src={raffle} alt="Raffle Page" />
+                                    <p>
+                                        Lottery powered by the Ethereum blockchain and Chainlink's decentralised network of oracles.
+                                    </p>
+                                    <p>
+                                        Available on the Sepolia testnet.
+                                    </p>
+                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go To Website</a>
+                        </div>
+
+                        <div className="projects__card">
+                            <h3>NFT Tickets</h3>
+                                <img src={tickets} alt="NFT Ticketing" />
+                                    <p>
+                                        Create events (Concerts, Conferences, Festivals, Sports, etc), create ticket categories, buy tickets created as NFTs.
+                                    </p>
+                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Available Soon</a>
                         </div>
 
                         <div className="projects__card">
@@ -45,21 +67,8 @@ const Projects = () => {
                                     <p>
                                         Available on Ethereum mainnet.
                                     </p>
-                                <a href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
+                                <a href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go To Website</a>
                         </div>
-
-                        <div className="projects__card">
-                            <h3>Lottery!</h3>
-                                <img src={raffle} alt="Raffle Page" />
-                                    <p>
-                                        Lottery powered by the Ethereum blockchain and Chainlink's decentralised network of oracles.
-                                    </p>
-                                    <p>
-                                        Available on the Sepolia testnet.
-                                    </p>
-                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go to website</a>
-                        </div>
-
 
                     </div>
 
