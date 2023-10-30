6a3569a7435b150730108c382fb741376e17a307
Vidhan Mangla <101626397+vidhanmangla@users.noreply.github.com>
2023-10-30 14:50:52 +0000

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,8 +7,6 @@ const Social = () => {
                 <a href="https://github.com/vidhanmangla" target="_blank" rel="noreferrer" className="button">GitHub</a>
                 <a href="https://twitter.com/vidhanmangla" target="_blank" rel="noreferrer" className="button">Twitter</a>
                 <a href="https://discord.gg/q5XtpBGWvb" target="_blank" rel="noreferrer" className="button">Discord</a>
-                <a href="https://www.instagram.com/_jizzy_b_" target="_blank" rel="noreferrer" className="button">Racing</a>
-                <a href="https://www.instagram.com/fireside.on.an.island" target="_blank" rel="noreferrer" className="button">Music</a>
                 <a href="https://ethereum.stackexchange.com/users/104489/vidhan-mangla?tab=profile" target="_blank" rel="noreferrer" className="button">Stack Exchange</a>
         </div>
     )
