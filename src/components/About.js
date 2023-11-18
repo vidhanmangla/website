64cdd7411754ced20b0a466cf4b051d9f3769fae
Vidhan Mangla <vidhanmangla@gmail.com>
2023-11-18 15:19:13 +0000

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -4,18 +4,21 @@ const About = () => {
         <section align="center" className='about'>
             <h2 align="center">About Me</h2>
                 <p align="center">
-                                A Blockchain enthusiast with 2+ years of experience developing production grade decentralised
-                                applications on EVM-compatible blockchains, skilled in Smart Contracts, Web3 and Blockchain
-                                infrastructure development.
+                                A Blockchain enthusiast with 2+ years of experience developing production grade
+                                decentralised applications on EVM-compatible blockchains, skilled in Smart Contracts,
+                                Web3 and Blockchain infrastructure development.
                 </p>
                     <br></br>
                     <br></br>
                 <p align="center">
-                                Guitarist and motor-racing driver.
+                                Guitarist and motor-racing driver. I am a passionate man, and passion drives me.
+                                Everything I do in life, it's my passion!
                 </p>
                     <br></br>
+                    <br></br>
                 <p align="center">
-                                I am a passionate man, and passion drives me. Everything I do in life, it's my passion!
+                                On the side, I research cryptography, specifically Elliptic-curve and Zero-knowledge
+                                proofs.
                 </p>
         </section>
     )
