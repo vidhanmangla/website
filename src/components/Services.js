846e669b6dcd0084468620b41eb6ab5b17dfbbdf
Vidhan Mangla <vidhanmangla@gmail.com>
2023-11-22 06:45:02 +0000

    chore: edited

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,22 +5,13 @@ const Services = () => {
             <h2 align="center">Services I Provide</h2>
                 <p>
                                 <p align="center">
-                                                NFT Consulting & Development • Crypto Wallet Development • dApp Development
+                                                Private Blockchain Networks
+                                                • Blockchain Explorers
                                 </p>
                     <br></br>
                                 <p align="center">
-                                                DeFi Solutions Engineering • Hyperledger & R3 Corda Solutions Engineering
-                                                • Cryptocurrency Exchange Engineering
-                                </p>
-                    <br></br>
-                                <p align="center">
-                                                Custom Blockchain Development & Integration • Enterprise Blockchain Development
-                                                • Blockchain Consulting & Advisory
-                                </p>
-                    <br></br>
-                                <p align="center">
-                                                Blockchain Game Development • Smart Contract Development & Audit
-                                                • Blockchain Training For Tech & Business Insiders
+                                                Decentralised Oracle Networks
+                                                • Smart Contracts & Audit
                                 </p>
                 </p>
         </section>
