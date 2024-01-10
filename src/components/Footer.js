b022f1a1f445b45628fd512848ef38cb1ddf447e
vidhanmangla <101626397+vidhanmangla@users.noreply.github.com>
2024-01-11 03:21:17 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer align="center" className="footer">
                         <p><small>This Web 3.0 website is hosted on IPFS using peer-to-peer networking</small></p>  
                 <br></br>
-                        <p><small>Copyright © {new Date().getFullYear()} Vidhan Mangla.</small></p>
+                        <p><small>Copyright © 2022 - {new Date().getFullYear()} Vidhan Mangla.</small></p>
                         <small><a href ="https://link.storjshare.io/jxwdhp75vxdnkx3zgeeovvobtbqq/jobs-material%2Fbitcoin.pdf" target="_blank" rel="noreferrer" attributes-list>Bitcoin Whitepaper</a> | <a href ="https://link.storjshare.io/jxjrs5eyhosfwty3widjvna22a4q/jobs-material%2Fethereum.pdf" target="_blank" rel="noreferrer" attributes-list>Ethereum Yellowpaper</a></small>
         </footer> 
     )
