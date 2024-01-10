e5825fc4f63ec0dd0488328ed55c4c3f0ed3a326
ghapaaghupp <vidhanmangla@gmail.com>
2024-01-10 18:28:39 +0530

    chore: remove twitter from socials

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,7 +5,6 @@ const Social = () => {
             <h2 align="center">Socials</h2>
                 <a href="https://www.linkedin.com/in/vidhanmangla" target="_blank" rel="noreferrer" className="button">LinkedIn</a>
                 <a href="https://github.com/vidhanmangla" target="_blank" rel="noreferrer" className="button">GitHub</a>
-                <a href="https://twitter.com/vidhanmangla" target="_blank" rel="noreferrer" className="button">Twitter</a>
                 <a href="https://discord.gg/q5XtpBGWvb" target="_blank" rel="noreferrer" className="button">Discord</a>
                 <a href="https://ethereum.stackexchange.com/users/104489/vidhan-mangla?tab=profile" target="_blank" rel="noreferrer" className="button">Stack Exchange</a>
         </div>
