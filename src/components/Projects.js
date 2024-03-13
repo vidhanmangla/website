a1ec71fbd5826f94a3cc2804e0c30182019bc4cd
ghapaaghupp <vidhanmangla@gmail.com>
2024-03-14 03:43:58 +0530

    yo

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
-import marketplace from '../assets/marketplace.png'
-import raffle from '../assets/raffle.png'
 import zkp from '../assets/zkp.png'
+import blockchain from '../assets/blockchain.png'
+import oracle from '../assets/oracle.png'
 
 
 const Projects = () => {
@@ -10,33 +10,30 @@ const Projects = () => {
             <h2>Technical Projects</h2>
                     <div className="projects__cards">
                         <div className="projects__card">
-                            <h3>Lottery!</h3>
-                                <img src={raffle} alt="Raffle Page" />
+                            <h3>Private Blockchain Networks</h3>
+                                <img src={blockchain} alt="Raffle Page" />
                                     <p>
-                                        Powered by Ethereum, Chainlink VRF and Chainlink Automation Upkeep.
+                                        Complete setup (Ethereum based).
                                     </p>
+                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Contact To Enquire</a>
+                        </div>
+
+                        <div className="projects__card">
+                            <h3>Network of Oracles</h3>
+                                <img src={oracle} alt="oracle" />
                                     <p>
-                                        Available on the Sepolia testnet.
+                                        Complete setup (Ethereum based).
                                     </p>
-                                <a align="center" href="https://master.d13r93vvjico63.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go To Website</a>
+                                <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Contact To Enquire</a>
                         </div>
 
                         <div className="projects__card">
                             <h3>ZK-SNARK and ZK-STARK</h3>
                                 <img src={zkp} alt="zk proof" />
                                     <p>
-                                        Stealth project.
+                                        Stealth projects based on zk tech.
                                     </p>
                                 <a align="center" href="mailto:vidhanmangla@gmail.com" target="_blank" rel="noreferrer" className="button">Contact To Enquire</a>
-                        </div>
-
-                        <div className="projects__card">
-                            <h3>Marketplace</h3>
-                                <img src={marketplace} alt="Marketplace" />
-                                    <p>
-                                        Web3 marketplace to buy, sell and trade digital products on Ethereum mainnet.
-                                    </p>
-                                <a href="https://main.d2bivn1wue16sy.amplifyapp.com/" target="_blank" rel="noreferrer" className="button">Go To Website</a>
                         </div>
                     </div>
         </section>
