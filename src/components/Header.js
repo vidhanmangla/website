0391a485f7c53051b48d31a28dda992d4dd1e171
vidhanmangla <101626397+vidhanmangla@users.noreply.github.com>
2024-03-25 01:02:39 +0000

    yo

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,11 +17,6 @@ const Header = () => {
                                     <a href='https://calendly.com/vidhanmangla/meeting-with-vidhan' target="_blank" rel="noreferrer" className='button'>Meeting</a>
                             <br></br>
                             <br></br>
-                                    <small><a href='https://link.storjshare.io/s/jw4nyfnj6iotmxdylpdhqwpetj2a/jobs-material%2FResume.pdf' target="_blank" rel="noreferrer">Resume</a></small>
-                            <br></br>
-                                    <small><a href='https://link.storjshare.io/s/jux7pgcwqd4etasc4luvfil55pjq/jobs-material%2FCover%20Letter.pdf' target="_blank" rel="noreferrer">Cover Letter</a></small>
-                            <br></br>
-                            <br></br>
                                     <small>📍 London, England, United Kingdom</small>
                 </div>
         </section>
