b33ced0ae311bf85d808cae22abf74d766e7ec65
ghapaaghupp <vidhanmangla@gmail.com>
2024-05-04 13:42:42 +0530

    chore: add blog post 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,9 +4,10 @@ import post1 from '../posts/post1';
 import post2 from '../posts/post2';
 import post3 from '../posts/post3';
 import post4 from '../posts/post4';
+import post5 from '../posts/post5';
 
 
-const blogPosts = [post1, post2, post3, post4];
+const blogPosts = [post5, post4, post3, post2, post1];
 
 
 const Blog = () => {
