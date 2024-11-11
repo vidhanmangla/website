77eef57e3e3659f77f082a54b238ac4d8d0565d5
vidhanmangla <vidhanmangla@gmail.com>
2024-11-11 07:37:44 +0530

    chore: add post7

---
+++ src/posts/post7.js
@@ -0,0 +1,7 @@
+const post = {
+    date: "October 21, 2024",
+    title: "Tokenising Real-World Assets: Bridging Blockchain Innovation with Legal Reality",
+    content: "Imagine Zulu, a small coffee shop owner whose business is struggling. To raise funds, Zulu has a clever idea: tokenise his 100-square-foot shop on the Ethereum blockchain. He splits the shop into 1,000 fungible tokens and sells each for £100, giving buyers fractional ownership of his business. But here’s the catch: what’s stopping Zulu from playing dirty? He could sell the shop in the real world, bypassing token holders entirely. Just because an asset is represented on-chain doesn’t guarantee off-chain ownership will be respected. Without proper legal or technological safeguards, the tokenisation of real-world assets can become meaningless. No matter how decentralised a blockchain is, tokenisation can’t circumvent legal systems that govern physical assets. Achieving full decentralisation is challenging, but there’s room to improve enforceability by combining centralised and decentralised approaches. Imagine a government-run private blockchain managing property records that interacts cross-chain with public blockchains like Ethereum. The property record remains within the jurisdiction of the state, while ownership rights are transparently reflected on public blockchains worldwide. Private blockchains, which I once doubted, can find a meaningful role here. They can bridge the gap between centralised control and decentralised trust, especially when managing real-world assets."
+};
+
+export default post;
